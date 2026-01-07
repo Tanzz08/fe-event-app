4c1a3d2326c6ae856de8c68824b05eeafb9b529e
tanzz <bustanuldarking23@gmail.com>
2026-01-07 11:50:05 +0800

    feat: user activation

---
+++ src/pages/auth/register/success/index.tsx
@@ -0,0 +1,13 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+import Register from "@/components/views/Auth/Register";
+import RegisterSuccess from "@/components/views/Auth/RegisterSuccess/RegisterSuccess";
+
+const RegisterSuccessPage = () => {
+  return (
+    <AuthLayout title="Acara | Register Success">
+      <RegisterSuccess />
+    </AuthLayout>
+  );
+};
+
+export default RegisterSuccessPage;
